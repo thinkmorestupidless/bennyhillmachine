74a3d88b37aee660b4adf9107c2de6453d3f9c9a
Trevor Burton-McCreadie <trevorburton-mccreadie@trevors-mbp.home>
2022-01-10 13:57:48 +0000

    added conversion of database url scheme (heroku and sqlalchemy don't play nice together without it)

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    database_path = os.getenv("DATABASE_URL")  # or other relevant config var
+    if database_path.startswith("postgres://"):
+        database_path = database_path.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = database_path
     YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 
 
